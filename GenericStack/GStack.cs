7eb6abc807e8861a81755aa32fc5f8a1b46ec9bb
pavel Švec <pavel.svec@gmail.com>
2021-09-13 13:33:25 +0200

    Přidejte soubory projektu.

---
+++ GenericStack/GStack.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericStack
+{
+    class Stack<T>
+    {
+        T[] elements;
+        int index;
+
+        public Stack(int maxElementCount = 100)
+        {
+            elements = new T[maxElementCount];
+            index = -1;
+        }
+    }
+
+
+}
---
+++ GenericStack/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> sch = new Stack<char>();
+        }
+    }
+}
